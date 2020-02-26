cfbd56c03ba2dbfa0fb89f3989277655f21674e8
Andrew Yushkewich <phorwix@gmail.com>
2020-02-26 19:30:52 +0300

    fix: trapezium

---
+++ src/labOOP/shapes/Trapezium.java
@@ -0,0 +1,25 @@
+package labOOP.shapes;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class Trapezium implements Shape {
+    private Point firstPoint, secondPoint, thirdPoint, fourthPoint;
+
+    public Trapezium(Point firstPoint, Point secondPoint, Point thirdPoint, Point fourthPoint) {
+        this.firstPoint = firstPoint;
+        this.secondPoint = secondPoint;
+        this.thirdPoint = thirdPoint;
+        this.fourthPoint = fourthPoint;
+    }
+
+    public void draw(GraphicsContext gc) {
+        gc.beginPath();
+        gc.moveTo(firstPoint.getX(), firstPoint.getY());
+        gc.lineTo(secondPoint.getX(), secondPoint.getY());
+        gc.lineTo(thirdPoint.getX(), thirdPoint.getY());
+        gc.lineTo(fourthPoint.getX(), fourthPoint.getY());
+        gc.lineTo(firstPoint.getX(), firstPoint.getY());
+        gc.closePath();
+        gc.stroke();
+    }
+}
---
+++ src/labOOP/shapes/TrapeziumCreator.java
@@ -0,0 +1,64 @@
+package labOOP.shapes;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import labOOP.ShapesList;
+
+public class TrapeziumCreator extends ShapeCreator {
+    enum State {waiting, firstPointSet, secondPointSet, thirdPointSet}
+
+    private Point firstPoint, secondPoint, thirdPoint, fourthPoint;
+    private State state;
+    private String name = "Trapezium";
+
+    public String getName() {
+        return name;
+    }
+
+    public TrapeziumCreator(GraphicsContext gc, ShapesList shapeList) {
+        super(gc);
+        state = State.waiting;
+        this.shapeList = shapeList;
+    }
+
+    public void dispatch(MouseEvent e) {
+        switch (state) {
+            case waiting: {
+                firstPoint = new Point(e.getX(), e.getY());
+                state = State.firstPointSet;
+                break;
+            }
+            case firstPointSet: {
+                secondPoint = new Point(e.getX(), e.getY());
+                state = State.secondPointSet;
+                break;
+            }
+            case secondPointSet: {
+                thirdPoint = new Point(e.getX(), e.getY());
+                state = State.thirdPointSet;
+                break;
+            }
+            case thirdPointSet: {
+                var x1 = firstPoint.getX();
+                var x2 = secondPoint.getX();
+                var x3 = thirdPoint.getX();
+                var x4 = e.getX();
+                var y1 = firstPoint.getY();
+                var y2 = secondPoint.getY();
+                var y3 = thirdPoint.getY();
+                var y4 = e.getY();
+
+                var lambda = ((x4 - x3) * (x2 - x1) + (y4 - y3) * (y2 - y1)) / (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+                var x = lambda * (x2 - x1) + x3;
+                var y = lambda * (y2 - y1) + y3;
+
+                fourthPoint = new Point(x, y);
+                var shape = new Trapezium(firstPoint, secondPoint, thirdPoint, fourthPoint);
+                shapeList.add(shape, gc);
+                state = State.waiting;
+                break;
+            }
+        }
+    }
+}
